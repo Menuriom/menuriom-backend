8035864ac1ee87ea93df6447568690dabcaad07c
amin <amin@amin.com>
2023-04-18 21:59:11 +0330

    add invites Model

---
+++ src/models/Invites.schema.ts
@@ -0,0 +1,31 @@
+import { Document, Schema } from "mongoose";
+import { Branch } from "./Branches.schema";
+import { Brand } from "./Brands.schema";
+export type InviteDocument = Invite & Document;
+
+export const InviteSchema = new Schema({
+    email: { type: String, lowercase: true },
+    mobile: { type: String },
+    branch: { type: Schema.Types.ObjectId, ref: "Branch", required: true },
+    brand: { type: Schema.Types.ObjectId, ref: "Brand", required: true },
+    status: {
+        type: String,
+        enum: ["sended", "canceled"],
+        default: "sended",
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(Date.now()),
+    },
+});
+
+export interface Invite {
+    _id: Schema.Types.ObjectId;
+    email: string;
+    mobile?: string;
+    branch?: Branch | Schema.Types.ObjectId;
+    brand?: Brand | Schema.Types.ObjectId;
+    status: "sended" | "canceled";
+    createdAt: Date;
+}
